968dd5f0c1c9f3934b76378389c185e1c99b0737
Tomoya <tomo16185@gmail.com>
2023-03-09 21:14:46 +0900

    #32 :bug: 従業員更新時にtime.Time型のデフォルト地が入ってきていたため、gorm.DeletedAtに型変更

--- api/infrastructure/dto/admin.go
+++ api/infrastructure/dto/admin.go
@@ -5,14 +5,16 @@ package dto
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Admin struct {
-	ID        uint      `gorm:"primary_key"`
-	Name      string    `gorm:"not null"`
-	Email     string    `gorm:"unique;not null"`
-	Password  string    `gorm:"not null"`
-	CreateAt  time.Time `gorm:"default:current_timestamp"`
-	UpdatedAt time.Time `gorm:"default:current_timestamp"`
-	DeletedAt time.Time `gorm:"default:null"`
+	ID        uint           `gorm:"primary_key"`
+	Name      string         `gorm:"not null"`
+	Email     string         `gorm:"unique;not null"`
+	Password  string         `gorm:"not null"`
+	CreateAt  time.Time      `gorm:"default:current_timestamp"`
+	UpdatedAt time.Time      `gorm:"default:current_timestamp"`
+	DeletedAt gorm.DeletedAt `gorm:"default:null"`
 }
